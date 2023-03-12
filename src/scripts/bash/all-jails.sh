93a8eb0481bb910a1bd4a5dae31d619554708878
Rob Seder <robseder@live.com>
2023-03-12 00:49:20 -0500

    Update all-jails.sh

--- src/scripts/bash/all-jails.sh
+++ src/scripts/bash/all-jails.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-JAILS=`fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g'`
+JAILS=$(fail2ban-client status | grep "Jail list" | sed -E 's/^[^:]+:[ \t]+//' | sed 's/,//g')
+INDEX=1
 for JAIL in $JAILS
 do
+  echo ""
+  echo -n "${INDEX}) "
   fail2ban-client status $JAIL
+  ((INDEX++))
 done
